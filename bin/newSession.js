5ea51a831e45db25633bbff185c1497096a6d4ec
Kylee Tilley <kyleetilley@gmail.com>
2017-12-24 18:18:56 -0600

    Refactoring

--- bin/newSession.js
+++ bin/newSession.js
@@ -7,11 +7,7 @@ const mf = require("../dist/mindflayer");
 
   const driver = new mf.WebDriver(`http://localhost:9515`, desiredCapabilities);
 
-  try {
-    await driver.start();
-    await driver.go("http://google.com");
-    await driver.close();
-  } catch (e) {
-    console.error(`Error going to url: ${e}`);
-  }
+  await driver.start();
+  await driver.go("http://google.com");
+  await driver.close();
 })();
